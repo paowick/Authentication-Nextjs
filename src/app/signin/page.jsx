df1a3e2da5bf937b112a5e6041085ebaf52cb257
paowick <korn2k9@gmail.com>
2024-09-08 20:32:01 +0700

    signin

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,21 +1,43 @@
 "use client";
-import React from "react";
-import { useFormState } from "react-dom";
+import React, { useState } from "react";
 import Link from "next/link";
-import { signin } from "./signin";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function page() {
-  const [state, signinAction] = useFormState(signin, { message: "" });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email,
+        password,
+      });
+
+      if (result.error) {
+        console.error(result.error);
+      } else {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <>
       <div style={{ maxWidth: "400px", margin: "auto", padding: "20px" }}>
         <h2>Sign In</h2>
-        <form action={signinAction}>
+        <form onSubmit={handleSubmit}>
           <div style={{ marginBottom: "15px" }}>
             <label htmlFor="email">Email</label>
             <input
               type="text"
               name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
               style={{ width: "100%", padding: "8px" }}
             />
@@ -25,11 +47,12 @@ export default function page() {
             <input
               type="password"
               name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               required
               style={{ width: "100%", padding: "8px" }}
             />
           </div>
-          <p aria-live="polite">{state?.message}</p>
           <button type="submit" style={{ padding: "10px 20px" }}>
             Signin
           </button>
