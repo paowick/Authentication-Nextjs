30178a257fa81c0bab1ee47c6b3b7e014c8fa911
paowick <korn2k9@gmail.com>
2024-09-24 12:36:41 +0700

    init user mgmt

--- src/app/admin/user-mgmt/page.jsx
+++ src/app/admin/user-mgmt/page.jsx
@@ -1,11 +1,16 @@
 import AdminNavbar from '@/app/components/AdminNavbar'
+import UserTable from '@/app/components/UserTable'
 import React from 'react'
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export default function UserManagement() {
+export default async function UserManagement() {
+  const users = await prisma.user.findMany()
   return (
     <>
     <div>UserManagement</div>
     <AdminNavbar/>
+    <UserTable users={users}/>
     </>
   )
 }
---
+++ src/app/components/UserTable.jsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+export default function UserTable({ users }) {
+  return (
+    <div>
+      <h1>User List</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
