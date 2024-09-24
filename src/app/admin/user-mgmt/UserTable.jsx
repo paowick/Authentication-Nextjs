bb52e2e2ef42fadac4f6993712309606a9c2dc1e
paowick <korn2k9@gmail.com>
2024-09-24 16:21:29 +0700

    modified:   src/app/admin/user-mgmt/UserTable.jsx

--- src/app/admin/user-mgmt/UserTable.jsx
+++ src/app/admin/user-mgmt/UserTable.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { Deleteuser } from "./Deleteuser";
 export default function UserTable({ users }) {
   const router = useRouter();
@@ -14,18 +14,27 @@ export default function UserTable({ users }) {
             <th>Name</th>
             <th>Email</th>
             <th>role</th>
-            <th></th>
-            <th></th>
+            <th>Reg Date</th>
           </tr>
         </thead>
         <tbody>
           {users.map((user) => (
             <tr key={user.id}>
+              <td>{user.id}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>{user.role}</td>
-              <td><button onClick={()=> router.push(`/admin/user-mgmt/edit/${user.id}`)} >Edit</button></td>
-              <td><button onClick={()=> Deleteuser(user.id)} >Delete</button></td>
+              <td>{new Date(user.createdAt).toLocaleString()}</td>
+              <td>
+                <button
+                  onClick={() =>
+                    router.push(`/admin/user-mgmt/edit/${user.id}`)
+                  }
+                >
+                  Edit
+                </button>
+                <button onClick={() => Deleteuser(user.id)}>Delete</button>
+              </td>
             </tr>
           ))}
         </tbody>
