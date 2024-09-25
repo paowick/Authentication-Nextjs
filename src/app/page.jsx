0a4c2c3215656b33b787a22b7467228107a7f282
paowick <korn2k9@gmail.com>
2024-09-26 00:01:44 +0700

    bug fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,23 @@
 'use client'
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(session);
+
+    if (status === "unauthenticated") {
+      router.push("/signin");
+    }
+    if (session?.user.name === null) {
+      router.push("/userinit");
+    }
+  }, [status, router, session]);
   return (
     <>first page</>
     )
