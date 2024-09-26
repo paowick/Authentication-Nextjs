75c65439b49774efec8f30cb70b997ef90cbc995
paowick <korn2k9@gmail.com>
2024-09-27 02:08:09 +0700

    bugfixed

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -4,8 +4,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from 'next/navigation'
 export default async function Page() {
   const session = await getServerSession(authOptions);
+  if(!session?.user){
+    redirect('/signin')
+  }
   const user = await prisma.user.findUnique({
     where: { id: session.user.id },
   });
